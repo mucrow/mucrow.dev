254d20afb9bbdcf18ed6767f3f1398702df33237
mucrow <120820315+mucrow@users.noreply.github.com>
2023-08-15 12:29:39 -0400

    Delete unused components

--- app/Link.tsx
+++
@@ -1,7 +0,0 @@
-import {AnchorHTMLAttributes} from 'react';
-
-import UnstyledLink from '@/app/UnstyledLink';
-
-export default function Link(props: React.PropsWithChildren<AnchorHTMLAttributes<HTMLAnchorElement>>) {
-  return <UnstyledLink {...props}>{props.children}</UnstyledLink>;
-}
--- app/UnstyledLink.tsx
+++
@@ -1,5 +0,0 @@
-import {AnchorHTMLAttributes} from 'react';
-
-export default function UnstyledLink(props: React.PropsWithChildren<AnchorHTMLAttributes<HTMLAnchorElement>>) {
-  return <a {...props}>{props.children}</a>;
-}
